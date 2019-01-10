8e6b87f5a58a7d9b1ca74b8569f054eef3a201e4
Alberto Navarro <alberto.cartagena@gmail.com>
2019-01-10 13:33:31 +0100

    changed array treatment/implementation

--- src/main/kotlin/com/navid/loggergenerator/JavaCodegen.kt
+++ src/main/kotlin/com/navid/loggergenerator/JavaCodegen.kt
@@ -58,7 +58,7 @@ fun generateJavaFile(mappingConfig: MappingConfig,
 
     val javaFile = JavaFile
             .builder(packageName, genClass.build())
-            .addStaticImport(saClassName, "*")
+            .addStaticImport(saClassName, "kv")
             .build()
 
     javaFile.writeTo(File(outputFolder))
@@ -145,7 +145,7 @@ fun createListFunction(entry: MappingEntry): MethodSpec {
             .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
             .addParameter(iterableType, entry.name)
             .returns(saClass)
-            .addStatement("return array(\$S,\$L)", entry.name, entry.name)
+            .addStatement("return new ObjectAppendingMarker(\$S,\$L)", entry.name, entry.name)
             .build()
 }
 
@@ -154,7 +154,7 @@ fun createVarargFunction(entry: MappingEntry): MethodSpec {
             .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
             .addParameter(ArrayTypeName.of(getTypeName(entry.type)), entry.name).varargs()
             .returns(saClass)
-            .addStatement("return array(\$S,\$L)", entry.name, entry.name)
+            .addStatement("return new ObjectAppendingMarker(\$S,\$L)", entry.name, entry.name)
             .build()
 }
 
